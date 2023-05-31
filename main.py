39dd5c62b3b06ecd72b59e9f705caa0f15c17d49
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 14:06:14 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def find_min_max(*args):
-    if not args:
-        return None
-
-    min_value = float('inf')
-    max_value = float('-inf')
-
-    for num in args:
-        if num < min_value:
-            min_value = num
-        if num > max_value:
-            max_value = num
-
-    return min_value, max_value
+def calculate_squares(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        result[key] = value ** 2
+    return result
