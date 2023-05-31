d05297b3ffbc8007e00ebe2a682699d949a52b02
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-31 14:05:18 +0300

    ex 2

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-result = concatenate_params(1, 'a', True, 'Hello')
-print(result)
+def find_min_max(*args):
+    if not args:
+        return None
+
+    min_value = float('inf')
+    max_value = float('-inf')
+
+    for num in args:
+        if num < min_value:
+            min_value = num
+        if num > max_value:
+            max_value = num
+
+    return min_value, max_value
